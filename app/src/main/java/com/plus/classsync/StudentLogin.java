30116acfa7e80106d345beb45557eb051ae3c324
Rafi <ahmed15-6037@gmail.com>
2024-05-14 00:04:56 +0600

    After CR and Student activity

--- app/src/main/java/com/plus/classsync/StudentLogin.java
+++ app/src/main/java/com/plus/classsync/StudentLogin.java
@@ -1,6 +1,8 @@
 package com.plus.classsync;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +11,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class StudentLogin extends AppCompatActivity {
-
+    Button studentLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_student_login);
+
+        studentLogin = findViewById(R.id.studentLogin);
+        studentLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(StudentLogin.this, SDashboard.class);
+            startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
